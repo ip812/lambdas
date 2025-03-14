16bff085711a342c4c42de1c0926ce75d2b06a4f
Ilia Petrov <ilia.yavorov.petrov@gmail.com>
2025-03-14 17:21:50 +0200

    feat: exec a query against local database

--- db-query-exec/go.mod
+++ db-query-exec/go.mod
@@ -8,6 +8,7 @@ require (
 )
 
 require (
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	golang.org/x/sys v0.31.0 // indirect
--- db-query-exec/main.go
+++ db-query-exec/main.go
@@ -2,14 +2,52 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"fmt"
+	"os"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	_ "github.com/lib/pq"
 )
 
-func Handler(ctx context.Context, event interface{}) (interface{}, error) {
+type QueryEvent struct {
+	DatabaseName string `json:"database_name"`
+	Query        string `json:"query"`
+}
+
+func Handler(ctx context.Context, event QueryEvent) (interface{}, error) {
 	log := logger(ctx)
-	log.Info().Msgf("Hello, received event is: %v", event)
-	return event, nil
+
+	log.Info().Msgf("event: %+v", event)
+
+	dbURL := fmt.Sprintf(
+		"postgres://%s:%s@%s:5432/%s?sslmode=disable",
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		event.DatabaseName,
+	)
+	log.Info().Msgf("dbURL: %s", dbURL)
+
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Info().Msgf("failed to connect to database: %s", err.Error())
+		return nil, err
+	}
+	defer db.Close()
+
+	row := db.QueryRowContext(ctx, event.Query)
+	var result string
+	err = row.Scan(&result)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return "", fmt.Errorf("no rows found")
+		}
+		return "", err
+	}
+
+	log.Info().Msgf("result: %s", result)
+	return result, nil
 }
 
 func main() {
